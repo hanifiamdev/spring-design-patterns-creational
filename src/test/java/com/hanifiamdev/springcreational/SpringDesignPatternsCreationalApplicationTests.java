657a8abb7820f846c31fa1c3ba6dda04b7631e39
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-09 03:17:21 +0700

    Membuat Project untuk implement design patterns creational

---
+++ src/test/java/com/hanifiamdev/springcreational/SpringDesignPatternsCreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.springcreational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsCreationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
